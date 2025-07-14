7d85f24b90ae5d7820abb0ae93fae5ab2bae0b4e
rohit2036 <157775775+rohit2036@users.noreply.github.com>
2025-07-14 15:09:39 +0530

    Update routes.py
    changes url_prefix="/api/v1"

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from .models import Student
 from . import db
 import logging
 
-api_v1 = Blueprint('api_v1', __name__)
+api_v1 = Blueprint('api_v1', __name__, url_prefix="/api/v1")
 
 @api_v1.route('/students', methods=['POST'])
 def create_student():
