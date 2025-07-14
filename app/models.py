9bb42db40f3768ea267eaa8c31b1978e9d4ba90f
rohit2036 <157775775+rohit2036@users.noreply.github.com>
2025-07-14 15:23:08 +0530

    Update models.py
    added space after class definition and removed spaces after line 11

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 
+
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -8,4 +9,3 @@ class Student(db.Model):
 
     def __repr__(self):
         return f'<Student {self.name}>'
-
